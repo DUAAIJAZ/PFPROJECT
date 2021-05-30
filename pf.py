fd33850383ed09eed3392513453bbd3fca35dd79
PFPROJECT <mifratanveer09@gmail.com>
2021-05-30 22:34:47 +0500

    2nd commit

--- pf.py
+++ pf.py
@@ -40,3 +40,56 @@ def winner(b, l):
             (b[0][2] == l and b[1][1] == l and b[2][0] == l))
  
 # Configure text on button while playing with another player
+def get_text(i, j, gb, l1, l2):
+
+      
+    global sign
+    if board[i][j] == ' ':
+        if sign % 2 == 0:
+            l1.config(state=DISABLED)
+            l2.config(state=ACTIVE)
+            board[i][j] = "X"
+        else:
+            l2.config(state=DISABLED)
+            l1.config(state=ACTIVE)
+            board[i][j] = "O"
+        sign += 1
+        button[i][j].config(text=board[i][j])
+    if winner(board, "X"):
+        gb.destroy()
+        box = messagebox.showinfo("Winner","mifra won the match")
+    elif winner(board, "O"):
+        gb.destroy()
+        box = messagebox.showinfo("Winner","dua won the match")
+    elif(isfull()):
+        gb.destroy()
+        box = messagebox.showinfo("Tie Game", "Tie Game")
+ 
+# Check if the player can push the button or not
+def isfree(i, j):
+    return board[i][j] == " "
+ 
+# Check the board is full or not
+def isfull():
+    flag = True
+    for i in board:
+        if(i.count(' ') > 0):
+            flag = False
+    return flag
+ 
+# Create the GUI of game board for play along with another player
+def gameboard_pl(game_board, l1, l2):
+    global button
+    button = []
+    for i in range(3):
+        m = 3+i
+        button.append(i)
+        button[i] = []
+        for j in range(3):
+            n = j
+            button[i].append(j)
+            get_t = partial(get_text, i, j, game_board, l1, l2)
+            button[i][j] = Button(
+                game_board, bd=5, command=get_t, height=4, width=8)
+            button[i][j].grid(row=m, column=n)
+    game_board.mainloop()
